7cee335b96b29ad2d970a8029a283ec7d3161c36
pboethig <pboethig@github.com>
2017-06-21 09:53:17 +0200

    add websocker async starter

---
+++ data/www/src/Mittax/MediaConverterBundle/Controller/WebsocketController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Mittax\MediaConverterBundle\Controller;
+
+use Jack\Symfony\ProcessManager;
+use Mittax\MediaConverterBundle\Service\Storage\Local\Upload;
+use Mittax\MediaConverterBundle\Service\System\Config;
+use Mittax\RabbitMQBundle\Service\Api\Queue;
+use Nelmio\ApiDocBundle\Annotation\ApiDoc;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\DependencyInjection\ContainerAwareTrait;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
+use Symfony\Component\HttpFoundation\ResponseHeaderBag;
+use Symfony\Component\Process\Process;
+
+
+class WebsocketController extends AbstractController
+{
+    /**
+     * @ApiDoc(
+     *  resource=true,
+     *  description="starts the websocket server",
+     *  section = "MediaConverter",
+     *  statusCodes={
+     *     200="Returned when successful",
+     *     500="retriving failed"
+     *  },
+     * )
+     * @Method("GET")
+     * @Route("/websocket/start")
+     */
+    public function startAction(Request $request)
+    {
+        $response = $this->getCoresResponse();
+
+        try
+        {
+            $proc_mgr = new ProcessManager();
+
+            $proc_mgr->runParallel([new Process('php /var/www/app/console gos:websocket:server')], 1, 1);
+
+            $response->setStatusCode(200);
+
+            $response->setContent('{"message":"success"}');
+
+            return $response;
+
+        }catch (\Exception $ex)
+        {
+            $response->setContent($ex->getMessage());
+
+            $response->setStatusCode(500);
+
+            return $response;
+        }
+    }
+}
